85c2ae09b374a47f4adf510ae95bc02d16585afd
marcel <marcelinseattle@gmail.com>
2017-04-05 20:30:02 -0700

    Add the business and user logic to the ping-pong project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-$(Blah! I suck at problem solving)
+    //user logic
+    $(document).ready(function() {
+      $("form#form1").submit(function(event) {
+          event.preventDefault();
+          var number = parseInt($("#input1").val());
+
+    //business logic
+    for (var index = 1; index <= number; index += 1) {
+      if ((index % 3 === 0) && (index % 5 === 0)) {
+        $("ul#list").append("<li>ping-pong</li>");
+      } else if (index % 3 === 0) {
+        $("ul#list").append("<li>ping</li>");
+      } else if (index % 5 === 0) {
+        $("ul#list").append("<li>pong</li>");
+      } else {
+        $("ul#list").append("<li>" + index + "</li>");
+      }
+    };
+    $("#result-div").show();
+  });
+});
