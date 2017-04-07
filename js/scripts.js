59c3bc65c27d99527c15ff88c55f484dbbc86dfd
marcel <marcelinseattle@gmail.com>
2017-04-06 19:33:07 -0700

    Add keeping the user and business logic separated

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
-    //user logic
-    $(document).ready(function() {
-      $("form#form1").submit(function(event) {
-          event.preventDefault();
-          var number = parseInt($("#input1").val());
+  //business logic
 
-    //business logic
-    for (var index = 1; index <= number; index += 1) {
+    function pingPong()  {
+
+    var number = parseInt($("#input1").val());
+      for (var index = 1; index <= number; index += 1) {
       if ((index % 3 === 0) && (index % 5 === 0)) {
         $("ul#list").append("<li>ping-pong</li>");
       } else if (index % 3 === 0) {
@@ -14,8 +12,16 @@
         $("ul#list").append("<li>pong</li>");
       } else {
         $("ul#list").append("<li>" + index + "</li>");
-      }
+      };
     };
-    $("#result-div").show();
+  };
+
+  //user logic
+
+  $(document).ready(function() {
+    $("form#form1").submit(function(event) {
+        event.preventDefault();
+        pingPong();
+        $("#result-div").show();
   });
 });
